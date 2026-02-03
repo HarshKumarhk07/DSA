a7781cb6dbc77de47880ee5d88568c89fb93f0c9
HarshKumarhk07 <harshkumarhk525@gmail.com>
2026-02-03 19:30:15 +0530

    Two Sum Problem

---
+++ Array/Two Pointers Array/TwoSum.java
@@ -0,0 +1,24 @@
+public class TwoSum {
+     public static int[] twoSum(int[] numbers, int target) {
+        int left=0;
+        int right=numbers.length-1;
+        while(left<right){
+            int sum = numbers[left]+numbers[right];
+            if(sum==target){
+                return new int[]{left+1,right+1};
+            }else if(sum<target){
+                left++;
+            }else if(sum>target){
+                right--;
+            }
+        }
+        return new int[0];
+    
+    }
+    public static void main(String[] args) { 
+        int[] arr = TwoSum.twoSum(new int[]{2,7,11,15}, 9);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }    
+}
