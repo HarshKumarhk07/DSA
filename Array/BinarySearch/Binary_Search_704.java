5e2f9401e18e4f4903642a1befffb7ce8ab642be
HarshKumarhk07 <harshkumarhk525@gmail.com>
2026-02-28 22:44:16 +0530

    704

---
+++ Array/BinarySearch/Binary_Search_704.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        return binarysearch(nums,target,0,nums.length-1);
+    }
+    public int binarysearch(int nums[],int target,int low,int high){
+        if(low>high){
+            return -1;
+        }
+        int mid = low+(high-low)/2;
+        if(nums[mid]==target){
+            return mid;
+        }else if(nums[mid]<target){
+            return binarysearch(nums,target,mid+1,high);
+        }
+
+        return binarysearch(nums,target,low,mid-1);
+    }
+}
---
+++ Stack/Reverse_a_Stack.java
@@ -0,0 +1,8 @@
+package Stack;
+
+import java.util.*;
+public class Reverse_a_Stack {
+    public static void main(String[] args) {
+        
+    }
+}
