a9cc0e076d30629450ac2a0531c5aecea5cfa049
HarshKumarhk07 <harshkumarhk525@gmail.com>
2026-03-10 22:57:05 +0530

    540  binary search leetcode

---
+++ Array/BinarySearch/Q540_Single_Element_in_a_Sorted_Array.java
@@ -0,0 +1,18 @@
+public class Q540_Single_Element_in_a_Sorted_Array {
+    public int singleNonDuplicate(int[] nums) {
+        int low =0;
+        int high = nums.length-1;
+        while(low<high){
+            int mid = low+(high-low)/2;
+                if(mid%2!=0){
+                    mid--;
+                }
+                if(nums[mid]==nums[mid+1]){
+                    low=mid+2;
+                }else{
+                    high=mid;
+                }
+        }
+        return nums[low];
+    }
+}
