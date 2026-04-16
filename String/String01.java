af9985df507a76fb11bf54b886154dfd3f40cbda
HarshKumarhk07 <harshkumarhk525@gmail.com>
2026-04-16 21:58:40 +0530

    String basics 01

---
+++ String/String01.java
@@ -0,0 +1,12 @@
+package String;
+import java.util.*;
+
+class String01{
+    public static void main(String arg[]){
+        Scanner sc = new Scanner(System.in);
+        String s = "hey man how are you ?";
+        String inpuString = sc.nextLine();
+        System.out.println(inpuString);
+
+    }
+}
